0b938df84143e4ef817ce7ed3e1ddc8d57c44f97
eduardoquintero117 <eduardoquiju@hotmail.com>
2023-08-03 23:25:51 -0600

    Initial Commit

---
+++ demoAppGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demoAppGit
+//
+//  Created by Eduardo Quintero on 03/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demoAppGit/demoAppGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoAppGitApp.swift
+//  demoAppGit
+//
+//  Created by Eduardo Quintero on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct demoAppGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
